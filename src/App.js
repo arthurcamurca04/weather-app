8fa506ed1a194266fc26c414902ef81d89249039
Arthur Camurça <arthur.04.pb@gmail.com>
2020-12-06 14:17:12 -0300

    Mudando api, adicionando os dados no front

--- src/App.js
+++ src/App.js
@@ -1,37 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./api";
 import "./styles.css";
 
 function App() {
+  const [citySearched, setCitySearched] = useState("");
+  const [cityName, setCityName] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [weatherText, setWeatherText] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [icon, setIcon] = useState("");
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    const res = await api.get(
+      `data/2.5/weather?q=${citySearched}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=metric&lang=pt_br`
+    );
+    console.log(res.data);
+    const data = {
+      city: res.data.name,
+      temp: res.data.main.temp,
+      text: res.data.weather[0].description,
+      hum: res.data.main.humidity,
+      ico: res.data.weather[0].icon,
+    };
+    setCityName(data.city);
+    setTemperature(data.temp);
+    setWeatherText(data.text);
+    setHumidity(data.hum);
+    setIcon(data.ico);
+
+    saveOnLocalStorage(data);
+  }
+
+  function saveOnLocalStorage(data) {
+    window.localStorage.setItem("dataWeather", JSON.stringify(data));
+  }
+
+  useEffect(() => {
+    const dataInfo = JSON.parse(window.localStorage.getItem("dataWeather"));
+
+    setCityName(dataInfo.city);
+    setTemperature(dataInfo.temp);
+    setWeatherText(dataInfo.text);
+    setHumidity(dataInfo.hum);
+    setIcon(dataInfo.ico);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather app</h1>
+        <h1>Weather React App</h1>
 
-        <form onSubmit={() => {}}>
+        <form onSubmit={handleFormSubmit}>
           <input
             type="search"
-            name="search"
-            id="search"
             placeholder="Digite o nome da cidade"
+            value={citySearched}
+            onChange={(event) => {
+              setCitySearched(event.target.value);
+            }}
           />
-          <button type="submit">Pesquisar</button>
+          <button type="submit">Buscar</button>
         </form>
       </header>
 
       <main>
         <div className="container">
-          <h1>Piancó</h1>
+          <h1>{cityName}</h1>
 
-          <span>36°</span>
-          <p>Céu limpo</p>
-          <hr/>
-          <p>Velocidade do vento: 5 km/h</p>
-          <p>Humidade: 32%</p>
+          <span>{Math.floor(temperature)} &#176;C</span>
+          <div className="weather-info">
+            <p>{weatherText.charAt(0).toUpperCase() + weatherText.slice(1)}</p>
+            <img
+              src={`http://api.openweathermap.org/img/w/${icon}.png`}
+              alt="Weather icon"
+            />
+          </div>
 
+          <hr />
+          <br />
+          <p>Humidade: {humidity} &#x0025;</p>
         </div>
       </main>
+      <footer>Made with &#x2764; by D3V Ninja</footer>
     </div>
   );
 }
-
 export default App;
--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://dataservice.accuweather.com",
+  baseURL: "http://api.openweathermap.org/",
 });
 
 export default api;
+
+// http://api.openweathermap.org/data/2.5/weather?q=Pianc%C3%B3&appid=22fa3512095c99b619c0cc3ec6c838ce&units=metric&lang=pt_br
